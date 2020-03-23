bbbcfa0c5f4cdecc9dc5ccb1616a04c35eab9fd1
tdobrowolski <tobdob15@gmail.com>
2020-03-23 18:29:58 +0100

    Initial Commit

---
+++ HeliumOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeliumOS
+//
+//  Created by Tobiasz Dobrowolski on 23/03/2020.
+//  Copyright © 2020 Tobiasz Dobrowolski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
